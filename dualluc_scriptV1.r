a4fb1faa99fb67576210b90d993d812b03ca1abb
Marc Galland <m.galland@uva.nl>
2020-03-06 09:59:58 +0100

    Attempt to use the gt package to output a nicely formatted FR1 table

--- dualluc_scriptV1.r
+++ dualluc_scriptV1.r
@@ -27,6 +27,8 @@ if (! "gt" %in% installed.packages()){
   remotes::install_github("rstudio/gt") # not available on CRAN yet
 }
 library(gt)
+library(webshot)
+# webshot::install_phantomjs() this needs to be run once
 
 
 #############
@@ -65,13 +67,21 @@ ggsave(filename = "FR_summary.png", width = 7, height = 5, dpi = 300)
 FR2_tidy2 <- FR_tidy %>%
   group_by(condition) %>%
   summarise(median = median(FR, na.rm = TRUE), mean = mean(FR, na.rm = TRUE))
-FR2_tidy2
 
 
+
+
+#######
+# Table
+#######
 # produce a table in PDF for publication containing the mean FR ratio per condition
-pdf("FR1.pdf", height=11, width=10)
-grid.table(FR2_tidy2)
-dev.off()
+# Use of the RStudio gt table package
+FR2_tidy2 %>% 
+  gt() %>% 
+  tab_header(title = "Firefly to renilla ratios") %>% 
+  gtsave(filename = "FR1.pdf")
+
+
 
 #doing the relative calculation i.e. normalize to expression of the empty vector "Ev"
 cond1 <- FR$B/FR$A
