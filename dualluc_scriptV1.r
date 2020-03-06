223620ab13f2cd07561db1ea3018a507cf742524
Marc Galland <m.galland@uva.nl>
2020-03-06 09:51:19 +0100

    Add the checkpoint library to manage packages. It makes sure we are all working with the same library versions. It slows down the execution of the script a bit but makes sure it works....compromise!

--- dualluc_scriptV1.r
+++ dualluc_scriptV1.r
@@ -7,49 +7,70 @@
 # level 2 calculations: normalize expression of experimental to control condition
 # plot relative expression 
 
+###########
+# Library
+###########
+if (! "checkpoint" %in% installed.packages()){
+  install.packages("checkpoint")
+}
+
+library("checkpoint")
+checkpoint("2020-01-01")
+
 # load the relevant libraries
 library(readxl)
-library(tidyr)
-library(ggplot2)
-library(dplyr)
+library(tidyverse)
 library(gridExtra)
 library(magrittr)
+library(remotes)
+if (! "gt" %in% installed.packages()){
+  remotes::install_github("rstudio/gt") # not available on CRAN yet
+}
+library(gt)
 
-# import excel output GloMax with readxl into a dataframe.The datasheet must be prepared by creating separate sheets for firefly and renilla.
+
+#############
+# Data import
+#############
+
+# import excel output GloMax with readxl into a dataframe.
+# The datasheet must be prepared by creating separate sheets for firefly and renilla.
 # import both data into separate dataframes
 #  either point to number or name of the correct sheet 
 firefly <- read_excel("DualReporter_example_data.xlsx", sheet = "firefly")
 renilla <- read_excel("DualReporter_example_data.xlsx", sheet = "renilla")
 
 
-#normalization to internal control
+# normalization to internal control
 FR <- firefly / renilla
 
 # convert dataframe to tidy format
 FR_tidy <- gather(FR, condition, FR)
 
-#plotting FR ratios for data overview
+#######
+# Plots
+#######
+# plotting FR ratios for data overview
 p1 <- ggplot(FR_tidy, aes(x = condition, y = FR)) +
   geom_jitter(position=position_jitter(0.1), cex=2, color="grey40") +
   stat_summary(fun.y = median, fun.ymin = median, fun.ymax = median,
                geom = "crossbar", width = 0.2)
 
 p1
-png("FR_summary.png", width = 300, height = 300)
-plot(p1)
-dev.off()
+
+ggsave(filename = "FR_summary.png", width = 7, height = 5, dpi = 300)
 
 
-#FR summary
+# FR summary
 FR2_tidy2 <- FR_tidy %>%
   group_by(condition) %>%
   summarise(median = median(FR, na.rm = TRUE), mean = mean(FR, na.rm = TRUE))
-FR_tidy2
+FR2_tidy2
 
 
-#produce a table in PDF for publication containing the mean FR ratio per condition
+# produce a table in PDF for publication containing the mean FR ratio per condition
 pdf("FR1.pdf", height=11, width=10)
-grid.table(FR_tidy2)
+grid.table(FR2_tidy2)
 dev.off()
 
 #doing the relative calculation i.e. normalize to expression of the empty vector "Ev"
